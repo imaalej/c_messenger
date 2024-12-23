5c1196ef8d601611d65a983895ab90b53ea878e4
imaalej <imaal081@uottawa.ca>
2024-12-23 17:37:13 -0500

    Changed processes to threads, added mutex lock for safety, enabled successful communication between clients, still some bugs to iron out.

--- server.c
+++ server.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,9 +16,11 @@
 void *get_in_addr(struct sockaddr *addr);
 void broadcast_message(int connections[], char msg[]);
 void add_connection(int connections[], int new_fd);
+void *handle_client(void *arg);
 
 int MAX_CONNECTIONS = 50;
 int num_clients;
+pthread_mutex_t mutex;
 
 int main(int argc, char *argv[]) {
 
@@ -25,9 +28,12 @@ int main(int argc, char *argv[]) {
   struct sockaddr_storage their_addr;
   int status, sockfd, b_status, l_status, new_fd;
   int connections[MAX_CONNECTIONS];
+  pthread_t threads[MAX_CONNECTIONS];
   socklen_t sin_size;
   char ipstr[INET6_ADDRSTRLEN]; // Holds the human readable IP
   char s[INET6_ADDRSTRLEN];
+
+  pthread_mutex_init(&mutex, NULL);
 
   memset(&hints, 0, sizeof hints); // Clear hints array
   hints.ai_family = AF_UNSPEC;     // Either IPv4 or IPv6
@@ -83,31 +89,61 @@ int main(int argc, char *argv[]) {
 
     printf("server: got connection from %s\n", s);
 
-    int id = fork();
+    void **args = malloc(2 * sizeof(void *));
 
-    if (id == -1) {
-      perror("server: fork");
+    if (args == NULL) {
+      perror("malloc");
+      close(new_fd);
       continue;
     }
 
-    if (id == 0) {
-      close(sockfd);
-      add_connection(connections, new_fd);
-      char recv_msg[1024];
-      while (1) {
-        memset(recv_msg, 0, sizeof(recv_msg));
-        ssize_t bytes_received;
-        bytes_received = recv(new_fd, &recv_msg, sizeof(recv_msg) - 1, 0);
-        if (bytes_received == -1) {
-          perror("recv");
-        }
-        recv_msg[bytes_received] = '\0';
-        broadcast_message(connections, recv_msg);
-      }
+    args[0] = &new_fd;
+    args[1] = connections;
+
+    if (pthread_create(&threads[num_clients], NULL, &handle_client,
+                       (void *)args) != 0) {
+      perror("pthread create");
+      close(new_fd);
+      continue;
     }
+    pthread_mutex_lock(&mutex);
+    num_clients++;
+    pthread_mutex_unlock(&mutex);
   }
 
+  pthread_mutex_destroy(&mutex);
+
   return EXIT_SUCCESS;
+}
+
+void *handle_client(void *arg) {
+  char recv_msg[1024];
+  void **args = (void **)arg;
+  int fd = *(int *)args[0];
+  int *connections = (int *)args[1];
+
+  pthread_mutex_lock(&mutex);
+  add_connection(connections, fd);
+  pthread_mutex_unlock(&mutex);
+
+  while (1) {
+    memset(recv_msg, 0, sizeof(recv_msg));
+    ssize_t bytes_received = recv(fd, &recv_msg, sizeof(recv_msg) - 1, 0);
+    if (bytes_received == -1) {
+      perror("recv");
+    }
+    recv_msg[bytes_received] = '\0';
+
+    pthread_mutex_lock(&mutex);
+    broadcast_message(connections, recv_msg);
+    pthread_mutex_unlock(&mutex);
+  }
+
+  // remove client
+  close(fd);
+  free(args[1]);
+  free(args);
+  return NULL;
 }
 
 void *get_in_addr(struct sockaddr *addr) {
@@ -129,7 +165,8 @@ void broadcast_message(int connections[], char msg[]) {
 
 void add_connection(int connections[], int new_fd) {
   if (num_clients < MAX_CONNECTIONS) {
-    connections[num_clients++] = new_fd;
+    // connections[num_clients++] = new_fd;
+    connections[num_clients] = new_fd;
     printf("Server: Added new connection.");
   } else {
     printf("Maximum client limit reached.");
